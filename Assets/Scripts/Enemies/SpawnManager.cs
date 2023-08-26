62e3555a53657b54eced7f293f188d5699cc9f05
Chris Williams <chris0n0williams@gmail.com>
2023-08-26 15:56:13 +0100

    Added Player tag to Player prefab

--- Assets/Scripts/Enemies/SpawnManager.cs
+++ Assets/Scripts/Enemies/SpawnManager.cs
@@ -6,15 +6,20 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private Transform playerSpawnPosition;
 
-    public GameObject antPrefab;
-    public int numberOfAnts = 100;
+    public GameObject ant_1Prefab;
+    public GameObject fireAntPrefab;
+    public int numberOfAnts_1 = 100;
+    public int numberOfFireAnts = 5;
 
-    private List<GameObject> ants = new List<GameObject>();
+    private List<GameObject> ants_1List = new List<GameObject>();
+    private List<GameObject> fireAntsList = new List<GameObject>();
+
 
     private void Start()
     {
-        InstantiateAnts();
+        InstantiateAnts_1();
         InstantiatePlayer();
+        InstantiateFireAnts();
     }
 
     private void InstantiatePlayer()
@@ -22,13 +27,23 @@ public class SpawnManager : MonoBehaviour
         GameObject player = Instantiate(playerPrefab, playerSpawnPosition.position, Quaternion.identity);
     }
 
-    private void InstantiateAnts()
+    private void InstantiateAnts_1()
     {
-        for (int i = 0; i < numberOfAnts; i++)
+        for (int i = 0; i < numberOfAnts_1; i++)
         {
             Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
-            GameObject ant = Instantiate(antPrefab, Random.insideUnitCircle * 10, randomRotation);
-            ants.Add(ant);
+            GameObject ant = Instantiate(ant_1Prefab, Random.insideUnitCircle * 10, randomRotation);
+            ants_1List.Add(ant);
+        }
+    }
+
+    private void InstantiateFireAnts()
+    {
+        for (int i = 0; i < numberOfFireAnts; i++)
+        {
+            Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
+            GameObject ant = Instantiate(fireAntPrefab, Random.insideUnitCircle * 10, randomRotation);
+            fireAntsList.Add(ant);
         }
     }
 }
